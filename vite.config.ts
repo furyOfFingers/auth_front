caa3069ff847fcf912e57f801bcac24e5924f399
furyOfFingers <maratformusic@gmail.com>
2025-03-28 16:02:12 +0300

    add chunks

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,22 @@ export default defineConfig({
     port: 5000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // Указываем адрес вашего бэкенда
+        target: "http://localhost:5000", // адрес бэкенда
         changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ''),  // Опционально: удаляет /api из пути
         secure: false, // Для локального HTTPS отключить проверку сертификата
       },
     },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          i18n: ["i18next", "react-i18next"],
+          mui: ["@mui/material"],
+          react: ["react", "react-dom"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 500,
   },
 });
